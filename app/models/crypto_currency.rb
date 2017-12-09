8c186bf7631bbbbcb2766794c8ff19c61a496055
tsurutan <ishaninaritaina623@gmail.com>
2017-12-09 20:57:26 +0900

    change background

--- app/models/crypto_currency.rb
+++ app/models/crypto_currency.rb
@@ -1,13 +1,13 @@
 class CryptoCurrency < ApplicationRecord
   extend FriendlyId
   friendly_id :name, use: :slugged
-  has_many :exchanges, through: :relation_exchange_crypto_currencies
   has_many :relation_exchange_crypto_currencies
+  has_many :exchanges, through: :relation_exchange_crypto_currencies
   accepts_nested_attributes_for :relation_exchange_crypto_currencies
   has_attached_file :icon_image,
                     :styles      => { :medium => "400x400>", :thumb => "100x100>" },
-                    :path => ":rails_root/public/system/:attachment/:id/:style/:filename",
-                    :url => "/system/:attachment/:id/:style/:filename"
+                    :path => ":rails_root/public/system/cryptocurrency_icon/:id/:style/:filename",
+                    :url => "/system/cryptocurrency_icon/:id/:style/:filename"
 
   validates_attachment :icon_image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png"] }
 
--- app/models/exchange.rb
+++ app/models/exchange.rb
@@ -2,20 +2,16 @@ class Exchange < ApplicationRecord
   extend FriendlyId
   friendly_id :name, use: :slugged
 
-  has_many :crypto_currencies, through: :relation_exchange_crypto_currencies
   has_many :relation_exchange_crypto_currencies
+  has_many :crypto_currencies, through: :relation_exchange_crypto_currencies
   has_many :ticker
 
   has_attached_file :icon_image,
                     :styles      => { :medium => "400x400>", :thumb => "100x100>" },
-                    :path => ":rails_root/public/system/:attachment/:id/:style/:filename",
+                    :path => ":rails_root/public/system/exchange_icon/:id/:style/:filename",
                     :url => "/system/:attachment/:id/:style/:filename"
 
   validates_attachment :icon_image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png"] }
-
-  def crypto_currencies
-    CryptoCurrency.find(relation_exchange_crypto_currencies.map {|relation| relation.crypto_currency_id})
-  end
 
   def tickers
     Ticker.find(ticker.ids)
